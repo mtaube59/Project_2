80d9f9a9b55dc6b4380a78944c3df0a6857d9c6d
julianafood <chojgy@gmail.com>
2019-04-29 14:46:08 -0400

    style

--- searches_table.sql
+++ searches_table.sql
@@ -1,8 +1,8 @@
-SELECT * FROM disaster_db.searches;
+SELECT * FROM disasters_db.searches;
 
-TRUNCATE TABLE disaster_db.searches;
+TRUNCATE TABLE disasters_db.searches;
 
-INSERT INTO disaster_db.searches (topic, name, count, createdAt, updatedAt) VALUES
+INSERT INTO disasters_db.searches (topic, name, count, createdAt, updatedAt) VALUES
  ('country', 'Nigeria', 1, curdate(), curdate()),
  ('type', 'Tornado', 1, curdate(), curdate()),
  ('country', 'Peru', 1, curdate(), curdate()),
