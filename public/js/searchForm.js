8710fd95b93f518dc697faf5578a53a67e17bb29
Sally Beckwith <sallypbeckwith@gmail.com>
2019-04-30 09:58:37 -0400

    fixed styling of charts. Refactored createChart function

--- public/js/searchForm.js
+++ public/js/searchForm.js
@@ -129,7 +129,7 @@ $("#search-disaster").click(function (event) {
           url: `/disasters/${search}`
         }).then(function (data) {
           var events = data.events;
-          var htmlstr;
+          var htmlstr = "";
           for (let i = 0; i < events.length; i++)  {
             htmlstr = htmlstr + `<h3 class="py-2 listed-event" data-id='${events[i].id}' data-desc='${events[i].description}'>${events[i].title}
             </h3>`
@@ -170,46 +170,3 @@ $("#search-disaster").click(function (event) {
     }
     return null;
   }
- 
-function createChart(labels, data, chartid) {
-
-  var ctx = document.getElementById(chartid).getContext('2d');
-  // var myChart = new Chart(ctx, {
-  var myChart = new Chart(ctx, {
-    type: 'polarArea',
-    data: {
-      labels: labels,
-      datasets: [{
-        label: "# of Search Requests",
-        data: data,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      // scales: {
-      //   yAxes: [{
-      //     ticks: {
-      //       beginAtZero: true
-      //     }
-      //   }]
-      // }
-    }
-  });
-  
-}
